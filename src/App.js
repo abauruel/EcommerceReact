3b2b7e41872569808e16022d9c4f0b42535519f5
abauruel <abauruel@gmail.com>
2019-12-21 11:18:21 -0300

    added navigation together sagas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
+import { ToastContainer } from 'react-toastify';
 import './config/ReactotronConfig';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import GlobalStyles from './styles/global';
 import Routes from './routes';
+import history from './services/history';
 
 import Header from './components/Header';
 // import { Container } from './styles';
@@ -14,11 +16,12 @@ import store from './store';
 export default function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <Router history={history}>
+        <ToastContainer autoClose={3000} />
         <GlobalStyles />
         <Header />
         <Routes />
-      </BrowserRouter>
+      </Router>
     </Provider>
   );
 }
