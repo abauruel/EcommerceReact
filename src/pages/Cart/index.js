11267479f14e36ad869963e0651d12874f246756
abauruel <abauruel@gmail.com>
2019-12-21 09:12:59 -0300

    added props total and subtotal on state

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -6,12 +6,12 @@ import {
   MdAddCircleOutline,
   MdDelete,
 } from 'react-icons/md';
-
+import { formatPrice } from '../../util/format';
 import { Container, ProductTable, Total } from './styles';
 
 import * as CartActions from '../../store/modules/cart/actions';
 
-function Cart({ cart, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmount }) {
   function Increment(product) {
     updateAmount(product.id, product.amount + 1);
   }
@@ -38,7 +38,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
               </td>
               <td>
                 <strong>tenis muito massa</strong>
-                <span>R$129,00</span>
+                <span>{product.priceFormated}</span>
               </td>
               <td>
                 <div>
@@ -52,7 +52,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
                 </div>
               </td>
               <td>
-                <strong>R$258,80</strong>
+                <strong>{product.subtotal}</strong>
               </td>
               <td>
                 <button
@@ -70,14 +70,22 @@ function Cart({ cart, removeFromCart, updateAmount }) {
         <button type="button">Finalizar Pedido</button>
         <Total>
           <span>TOTAL</span>
-          <strong>R$129,00</strong>
+          <strong>{total}</strong>
         </Total>
       </footer>
     </Container>
   );
 }
 const mapStateToProps = state => ({
-  cart: state.cart,
+  cart: state.cart.map(product => ({
+    ...product,
+    subtotal: formatPrice(product.price * product.amount),
+  })),
+  total: formatPrice(
+    state.cart.reduce((total, product) => {
+      return total + product.price * product.amount;
+    }, 0)
+  ),
 });
 const mapDispatchToProps = dispatch =>
   bindActionCreators(CartActions, dispatch);
